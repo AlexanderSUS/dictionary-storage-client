be1451a61cac5ff68dac977492d60d47640d4934
Alexander Suslov <sasha_suslov07@mail.ru>
2022-10-01 21:41:48 +0300

    feat: add links and style to header

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import HeaderNavLink from 'components/HeaderNavLink/HeaderNavLink';
+import { ABOUT_ROUTE, TEXT_ROUTE, WORD_ROUTE } from 'const/routes';
+import styles from './Header.module.scss';
 
 const Header = () => (
-  <header>Header</header>
+  <header className={styles.header}>
+    <nav>
+      <ul className={styles.navList}>
+        <li><HeaderNavLink to={WORD_ROUTE}>{WORD_ROUTE}</HeaderNavLink></li>
+        <li><HeaderNavLink to={TEXT_ROUTE}>{TEXT_ROUTE}</HeaderNavLink></li>
+        <li><HeaderNavLink to={ABOUT_ROUTE}>{ABOUT_ROUTE}</HeaderNavLink></li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default Header;

fe301b04a3f28c94eedc0d5aa5f2c3d05c52e1d7
Alexander Suslov <sasha_suslov07@mail.ru>
2022-10-14 05:54:05 +0300

    fix: text

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => (
         <li><HeaderNavLink to={WORD_ROUTE}>{WORD_ROUTE}</HeaderNavLink></li>
         <li><HeaderNavLink to={TEXT_ROUTE}>{TEXT_ROUTE}</HeaderNavLink></li>
         <li><HeaderNavLink to={ABOUT_ROUTE}>{ABOUT_ROUTE}</HeaderNavLink></li>
-        <li><HeaderNavLink to={SIGIN_UP_ROUTE}>Sign in</HeaderNavLink></li>
+        <li><HeaderNavLink to={SIGIN_UP_ROUTE}>Sign up</HeaderNavLink></li>
         <li><HeaderNavLink to={LOG_IN_ROUTE}>Log in</HeaderNavLink></li>
       </ul>
     </nav>
